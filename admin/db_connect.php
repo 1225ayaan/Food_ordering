226877fc4440f9d608e79d995f688beb58bf7024
1225ayaan <94957196+1225ayaan@users.noreply.github.com>
2024-02-22 23:03:56 -0800

    Update db_connect.php
    changed the connections

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','fos_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('ayaan0431.mysql.database.azure.com','myadmin@ayaan0431','','fos_db')or die("Could not connect to mysql".mysqli_error($con));
