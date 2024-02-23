5d9f4b6492befe5ff191b9945eff681567749926
1225ayaan <94957196+1225ayaan@users.noreply.github.com>
2024-02-22 23:23:46 -0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('ayaan0431.mysql.database.azure.com','myadmin@ayaan0431','','fos_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('ayaan0431.mysql.database.azure.com','myadmin@ayaan0431','aY#aan123','fos_db')or die("Could not connect to mysql".mysqli_error($con));
