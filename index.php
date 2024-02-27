e49d338d90e03370e94c49bd8e753477852692ac
1225ayaan <94957196+1225ayaan@users.noreply.github.com>
2024-02-27 15:00:43 +0530

    update

--- index.php
+++ index.php
@@ -136,7 +136,7 @@
         </div>
         <footer class="bg-light py-5">
             <div class="container">
-                <div class="small text-center text-muted">Copyright © 2024 - Ayaan Jahagirdar | <a href="#" target="_blank">Sourcecodester</a></div>
+                <div class="small text-center text-muted">Copyright © 2024 - Ayaan Jahagirdar | <a href="#" target="_blank"></a></div>
             </div>
         </footer>
         
