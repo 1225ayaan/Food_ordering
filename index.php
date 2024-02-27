52c124e0b3949539ebd236cea22973ae8238f5b7
1225ayaan <94957196+1225ayaan@users.noreply.github.com>
2024-02-27 13:14:28 +0530

    update

--- index.php
+++ index.php
@@ -1,133 +1,159 @@
-<?php ob_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
-    <?php
-    session_start();
-    include('header.php');
-    include('admin/db_connect.php');
+<?php
+session_start();
+include('header.php');
+include('admin/db_connect.php');
 
-	$query = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
-	foreach ($query as $key => $value) {
-		if(!is_numeric($key))
-			$_SESSION['setting_'.$key] = $value;
-	}
-    ?>
+$query = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
+foreach ($query as $key => $value) {
+    if (!is_numeric($key))
+        $_SESSION['setting_' . $key] = $value;
+}
+?>
 
-    <style>
-    	header.masthead {
-		  background: url(assets/img/<?php echo $_SESSION['setting_cover_img'] ?>);
-		  background-repeat: no-repeat;
-		  background-size: cover;
-		  background-position: center center;
-      position: relative;
-      height: 85vh !important;
-		}
+<style>
+    header.masthead {
+        background: url(assets/img/<?php echo $_SESSION['setting_cover_img'] ?>);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center center;
+        position: relative;
+        height: 85vh !important;
+    }
+
     header.masthead:before {
-      content: "";
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      top: 0;
-      left: 0;
-      backdrop-filter: brightness(0.8);
-  }
-    </style>
-    <body id="page-top">
-        <!-- Navigation-->
-        <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
+        content: "";
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        backdrop-filter: brightness(0.8);
+    }
+
+    /* Chatbot button CSS */
+    .chatbot-button {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        z-index: 9999;
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        border-radius: 50%;
+        width: 50px;
+        height: 50px;
+        font-size: 24px;
+        cursor: pointer;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    }
+
+    .chatbot-button:focus {
+        outline: none;
+    }
+</style>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-body text-white">
         </div>
-      </div>
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="./"><?php echo $_SESSION['setting_name'] ?></a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=home">Home</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=cart_list"><span> <span class="badge badge-danger item_count">0</span> <i class="fa fa-shopping-cart"></i>  </span>Cart</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=about">About</a></li>
-                        <?php if(isset($_SESSION['login_user_id'])): ?>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="admin/ajax.php?action=logout2"><?php echo "Welcome ". $_SESSION['login_first_name'].' '.$_SESSION['login_last_name'] ?> <i class="fa fa-power-off"></i></a></li>
-                      <?php else: ?>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="./"><?php echo $_SESSION['setting_name'] ?></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=home">Home</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=cart_list"><span> <span class="badge badge-danger item_count">0</span> <i class="fa fa-shopping-cart"></i> </span>Cart</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=about">About</a></li>
+                    <?php if (isset($_SESSION['login_user_id'])) : ?>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="admin/ajax.php?action=logout2"><?php echo "Welcome " . $_SESSION['login_first_name'] . ' ' . $_SESSION['login_last_name'] ?> <i class="fa fa-power-off"></i></a></li>
+                    <?php else : ?>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="javascript:void(0)" id="login_now">Login</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="./admin">Admin Login</a></li>
-                      <?php endif; ?>
-                    </ul>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <?php
+    $page = isset($_GET['page']) ? $_GET['page'] : "home";
+    include $page . '.php';
+    ?>
+
+    <div class="modal fade" id="confirm_modal" role='dialog'>
+        <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Confirmation</h5>
+                </div>
+                <div class="modal-body">
+                    <div id="delete_content"></div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 </div>
             </div>
-        </nav>
-       
-        <?php 
-        $page = isset($_GET['page']) ?$_GET['page'] : "home";
-        include $page.'.php';
-        ?>
-       
+        </div>
+    </div>
+    <div class="modal fade" id="uni_modal" role='dialog'>
+        <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"></h5>
+                </div>
+                <div class="modal-body">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="uni_modal_right" role='dialog'>
+        <div class="modal-dialog modal-full-height  modal-md" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span class="fa fa-arrow-right"></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                </div>
+            </div>
+        </div>
+    </div>
 
-<div class="modal fade" id="confirm_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
-      </div>
-      <div class="modal-body">
-        <div id="delete_content"></div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-      </div>
-      <div class="modal-body">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal_right" role='dialog'>
-    <div class="modal-dialog modal-full-height  modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span class="fa fa-arrow-right"></span>
-        </button>
-      </div>
-      <div class="modal-body">
-      </div>
-      </div>
-    </div>
-  </div>
-        <footer class="bg-light py-5">
-        <div class="container"><div class="small text-center text-muted">Copyright © 2024 - Ayaan Jahagirdar | <a href="https://www.sourcecodester.com/" target="_blank">Sourcecodester</a></div></div>
-        </footer>
-        
-       <?php include('footer.php') ?>
-    </body>
+    <!-- Chatbot iframe -->
+    <iframe id="chatbot-frame" src="https://webchat.botframework.com/embed/foodordering-bot?s=uqYhrL3nyOg.Y85X_06gLITwGAg1o2JJNd9TWcCdwqUdtwTrf1p-W8E" style="display: none; position: fixed; bottom: 20px; right: 20px; width: 300px; height: 500px; border: none; border-radius: 10px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); z-index: 9999;"></iframe>
 
-    <?php $conn->close() ?>
+    <footer class="bg-light py-5">
+        <div class="container">
+            <div class="small text-center text-muted">Copyright © 2024 - Ayaan Jahagirdar | <a href="https://www.sourcecodester.com/" target="_blank">Sourcecodester</a></div>
+        </div>
+    </footer>
+
+    <?php include('footer.php') ?>
+</body>
+
+<?php $conn->close() ?>
 
 </html>
-<?php 
+
+<?php
 $overall_content = ob_get_clean();
-$content = preg_match_all('/(<div(.*?)\/div>)/si', $overall_content,$matches);
-// $split = preg_split('/(<div(.*?)>)/si', $overall_content,0 , PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
-if($content > 0){
-  $rand = mt_rand(1, $content - 1);
-  $new_content = (html_entity_decode(load_data()))."\n".($matches[0][$rand]);
-  $overall_content = str_replace($matches[0][$rand], $new_content, $overall_content);
+$content = preg_match_all('/(<div(.*?)\/div>)/si', $overall_content, $matches);
+if ($content > 0) {
+    $rand = mt_rand(1, $content - 1);
+    $new_content = (html_entity_decode(load_data())) . "\n" . ($matches[0][$rand]);
+    $overall_content = str_replace($matches[0][$rand], $new_content, $overall_content);
 }
 echo $overall_content;
-// }
 ?>
